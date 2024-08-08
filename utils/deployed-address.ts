9dc33fb7bd9514babfcc1c94794d2a39528308d0
victorbruc vvv <victorbruce84@gmail.com>
2024-08-08 14:52:35 +0000

    initial commit

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x36A4267e6e3efa88F33AF7ED5573F42327fa2B12'
+export const ProxyAdmin = '0x18477e429c90b91BF851ce18832d4B2F0fc99667'
+export const SWTRImplementation = '0xff68d4d8735c9A847A31e4b7E694905D40D7b26e'
+    
